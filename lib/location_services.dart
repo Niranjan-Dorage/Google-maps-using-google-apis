4e7ddda3adca02ccf0cc2bb45b28e583cc7eb38c
Niranjan Dorage <niranjandorage@gmail.com>
2023-09-22 18:57:32 +0530

    minor changes

--- lib/location_services.dart
+++ lib/location_services.dart
@@ -1,6 +1,26 @@
-// class Locationservices {
-//   final String key = 'api_key';
-//   Future<String> getPlaceID(String input) async{
+// import 'dart:convert' as convert;
 
-//   } 
+// import 'package:http/http.dart' as http;
+
+// class Locationservices {
+//   final String key = 'AIzaSyBm4GCrErBOwkaArd-xhiXgNftCFj7V4HQ';
+//   static Future<String> getPlaceID(String input) async {
+//     String url =
+//         "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=mongolian&inputtype=textquery&key=AIzaSyBPKNqauOsPPJzyTjrksvOD-wrY33930hY";
+//     var response = await http.get(Uri.parse(url));
+//     var json = convert.jsonDecode(response.body);
+//     var placeid = json['candidates'][0]['place_id'] as String;
+  //   print(placeid);
+  //   return placeid;
+  // }
+//  Future<Map<String, dynamic>> getPlaces(String input) async {
+//     final placeID = getPlaceID(input);
+//     final String url =
+//         'https://maps.googleapis.com/maps/api/place/details/json?place_id=$placeID&fields=address_components&key=AIzaSyBPKNqauOsPPJzyTjrksvOD-wrY33930hY';
+//     var response = await http.get(Uri.parse(url));
+//     var json = convert.jsonDecode(response.body);
+//     var result = json['result'] as Map<String, dynamic>;
+//     return result;
 //   }
+
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
@@ -13,25 +12,29 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
+double sourcelat = 0.0;
+double sourcelng = 0.0;
+double destlat = 0.0;
+double destlng = 0.0;
+
 class _MyAppState extends State<MyApp> {
-  static Marker googlemaps = const Marker(
-      infoWindow: InfoWindow(title: 'google maps'),
-      icon: BitmapDescriptor.defaultMarker,
-      position: LatLng(19.0091366, 73.1039735),
-      markerId: MarkerId('googlemaps'));
-  static Marker googlemarker2 = Marker(
-      infoWindow: const InfoWindow(title: 'google marker'),
-      icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-      position: const LatLng(19.0091366, 73.1139735),
-      markerId: const MarkerId('googlemar'));
-  static Polyline polymaps = const Polyline(
-      polylineId: PolylineId('polymaps'),
-      color: Color.fromARGB(255, 18, 213, 0),
-      width: 1,
-      points: [LatLng(19.0091366, 73.1039735), LatLng(19.0091366, 73.1139735)]);
-  Map<PolylineId, Polyline> polylines = {};
-  List<LatLng> polylineCoordinates = [];
-  PolylinePoints polylinePoints = PolylinePoints();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  // void _addPolyline() {
+  //   setState(() {
+  //     // Locationservices.getPlaceID('Mumbai');
+  //     // _polylines.add(Polyline(
+  //     //   polylineId: PolylineId('polymaps'),
+  //     //   color: Colors.green,
+  //     //   width: 2,
+  //     //   points: [source, destination],
+  //     // ));
+  //   });
+  // }
+
   final LatLng _center = const LatLng(19.0091366, 73.1039735);
 
   late GoogleMapController mapController;
@@ -49,38 +52,278 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    Marker googlemaps = Marker(
+        infoWindow: const InfoWindow(title: 'google maps'),
+        icon: BitmapDescriptor.defaultMarker,
+        position: LatLng(sourcelat, sourcelng),
+        markerId: const MarkerId('googlemaps'));
+    Marker googlemarker2 = Marker(
+        infoWindow: const InfoWindow(title: 'google marker'),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+        position: LatLng(destlat, destlng),
+        markerId: const MarkerId('googlemar'));
+    final LatLng source = LatLng(sourcelat, sourcelng);
+    final LatLng destination = LatLng(destlat, destlng);
+    Size size = MediaQuery.of(context).size;
+
     return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-            appBar: AppBar(
-                backgroundColor: const Color.fromARGB(255, 48, 48, 48),
-                title: const Text('Sasta Google Maps')),
-            body: GoogleMap(
-              markers: {googlemaps, googlemarker2},
-              polylines: {polymaps},
-              myLocationButtonEnabled: false,
-              zoomControlsEnabled: false,
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 13,
-              ),
-              // markers: Set<Marker>.of(markers.values),
-              // polylines: Set<Polyline>.of(polylines.values),
-            ),
-            floatingActionButton: FloatingActionButton(
-              backgroundColor: const Color.fromARGB(255, 48, 48, 48),
-              child: const Icon(Icons.center_focus_strong),
-              onPressed: () async {
-                mapController.animateCamera(
-                  CameraUpdate.newCameraPosition(
-                    CameraPosition(
-                      target: _center,
-                      zoom: 18.0,
+        home: GestureDetector(
+          onTap: () {
+            FocusManager.instance.primaryFocus?.unfocus();
+          },
+          child: Scaffold(
+              backgroundColor: const Color.fromARGB(255, 197, 197, 197),
+              appBar: AppBar(
+                  backgroundColor: const Color.fromARGB(255, 48, 48, 48),
+                  title: const Text('Sasta Google Maps')),
+              body: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 20,
                     ),
-                  ),
-                );
-              },
-            )));
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: size.width / 4,
+                          margin: const EdgeInsets.all(10),
+                          child: const Text(
+                            "source :",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          height: 45,
+                          width: size.width / 4,
+                          padding: EdgeInsets.only(left: size.width / 15),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 231, 230, 230),
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                          child: TextField(
+                            onChanged: (value) {
+                              if (value != "") {
+                                sourcelat = double.tryParse(value) as double;
+                              } else if (value == "") {
+                                sourcelat = 0.0;
+                              }
+                              print(sourcelat);
+                            },
+                            cursorColor: const Color.fromARGB(55, 97, 97, 97),
+                            keyboardType: TextInputType.phone,
+                            style: const TextStyle(
+                              fontFamily: "roboto",
+                              fontSize: 16,
+                            ),
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                fontFamily: 'roboto',
+                              ),
+                              hintText: 'Lat',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          height: 45,
+                          width: size.width / 4,
+                          padding: EdgeInsets.only(left: size.width / 15),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 231, 230, 230),
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                          child: TextField(
+                            onChanged: (value) {
+                              if (value != "") {
+                                sourcelng = double.tryParse(value) as double;
+                              } else if (value == "") {
+                                sourcelng = 0.0;
+                              }
+                              print(sourcelng);
+                            },
+                            cursorColor: const Color.fromARGB(55, 97, 97, 97),
+                            keyboardType: TextInputType.phone,
+                            style: const TextStyle(
+                              fontFamily: "roboto",
+                              fontSize: 16,
+                            ),
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                fontFamily: 'roboto',
+                              ),
+                              hintText: 'Lng',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: size.width / 4,
+                          margin: const EdgeInsets.all(10),
+                          child: const Text(
+                            "Dest : ",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          height: 45,
+                          width: size.width / 4,
+                          padding: EdgeInsets.only(left: size.width / 15),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 231, 230, 230),
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                          child: TextField(
+                            onChanged: (value) {
+                              if (value != "") {
+                                destlat = double.tryParse(value) as double;
+                              } else if (value == "") {
+                                destlat = 0.0;
+                              }
+                              print(destlat);
+                            },
+                            cursorColor: const Color.fromARGB(55, 97, 97, 97),
+                            keyboardType: TextInputType.phone,
+                            style: const TextStyle(
+                              fontFamily: "roboto",
+                              fontSize: 16,
+                            ),
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                fontFamily: 'roboto',
+                              ),
+                              hintText: 'Lat',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(10),
+                          height: 45,
+                          width: size.width / 4,
+                          padding: EdgeInsets.only(left: size.width / 15),
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 231, 230, 230),
+                            borderRadius: BorderRadius.circular(7),
+                          ),
+                          child: TextField(
+                            onChanged: (value) {
+                              if (value != "") {
+                                destlng = double.tryParse(value) as double;
+                              } else if (value == "") {
+                                destlng = 0.0;
+                              }
+                              print(destlng);
+                            },
+                            cursorColor: const Color.fromARGB(55, 97, 97, 97),
+                            keyboardType: TextInputType.phone,
+                            style: const TextStyle(
+                              fontFamily: "roboto",
+                              fontSize: 16,
+                            ),
+                            decoration: const InputDecoration(
+                              hintStyle: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                fontFamily: 'roboto',
+                              ),
+                              hintText: 'Lng',
+                              border: InputBorder.none,
+                              contentPadding: EdgeInsets.all(10),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      child: GoogleMap(
+                        markers: {googlemaps, googlemarker2},
+                        polylines: {
+                          Polyline(
+                              polylineId: const PolylineId('route1'),
+                              color: const Color.fromARGB(255, 15, 224, 43),
+                              width: 2,
+                              points: [source, destination])
+                        },
+                        myLocationButtonEnabled: false,
+                        zoomControlsEnabled: false,
+                        onMapCreated: _onMapCreated,
+                        initialCameraPosition: CameraPosition(
+                          target: _center,
+                          zoom: 13,
+                        ),
+                        // markers: Set<Marker>.of(markers.values),
+                        // polylines: Set<Polyline>.of(polylines.values),
+                      ),
+                    ),
+                    Container(
+                        margin: const EdgeInsets.only(left: 20),
+                        alignment: Alignment.centerLeft,
+                        child: ElevatedButton(
+                            onPressed: () {
+                              setState(() {});
+                              mapController.animateCamera(
+                                CameraUpdate.newCameraPosition(
+                                  CameraPosition(
+                                    target: source,
+                                    zoom: 18.0,
+                                  ),
+                                ),
+                              );
+                            },
+                            child: SizedBox(
+                                width: 60,
+                                child: const Text(
+                                  'route',
+                                  textAlign: TextAlign.center,
+                                ))))
+                  ],
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                backgroundColor: const Color.fromARGB(255, 48, 48, 48),
+                child: const Icon(Icons.center_focus_strong),
+                onPressed: () async {
+                  mapController.animateCamera(
+                    CameraUpdate.newCameraPosition(
+                      CameraPosition(
+                        target: _center,
+                        zoom: 18.0,
+                      ),
+                    ),
+                  );
+                },
+              )),
+        ));
   }
 }
